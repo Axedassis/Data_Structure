8dcdac3b061b5c9f1c3a1667a4279f4c8f58a410
Lucas <Axedassis>
2024-09-12 22:52:02 -0300

    sortLinkedListStack

---
+++ LinearDataStructure/Stack/sortStackLinkedList.c
@@ -0,0 +1,66 @@
+/*
+ mmmm                  m            mmmm    m                  #            
+ #"   "  mmm    m mm  mm#mm         #"   " mm#mm   mmm    mmm   #   m        
+ "#mmm  #" "#   #"  "   #           "#mmm    #    "   #  #"  "  # m"         
+     "# #   #   #       #               "#   #    m"""#  #      #"#          
+ "mmm#" "#m#"   #       "mm         "mmm#"   "mm  "mm"#  "#mm"  #  "m        
+                                                                             
+                                                                             
+                                                                             
+ m       ""           #                 #         m        "             m   
+ #      mmm    m mm   #   m   mmm    mmm#         #      mmm     mmm   mm#mm 
+ #        #    #"  #  # m"   #"  #  #" "#         #        #    #   "    #   
+ #        #    #   #  #"#    #""""  #   #         #        #     """m    #   
+ #mmmmm mm#mm  #   #  #  "m  "#mm"  "#m##         #mmmmm mm#mm  "mmm"    "mm 
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+typedef struct NodeStack
+{
+	int data;
+	struct NodeStack *next;
+} NodeStack;
+
+NodeStack *createNode(int data)
+{
+	NodeStack *node = (NodeStack *)malloc(sizeof(NodeStack));
+	if(node == NULL)
+	{
+		printf("Unable to allocate memory");
+		exit (EXIT_FAILURE);
+	}
+	node->next = NULL;
+	node->data = data;
+	return (data);
+}
+
+void swap(NodeStack *bottomNode, NodeStack *topNode)
+{
+	NodeStack *tmp = bottomNode;
+	bottomNode->next = topNode->next;
+	topNode->next = tmp;
+}
+
+int isEmpty(NodeStack *root)
+{
+	return !root;
+}
+
+void push(NodeStack **root, int data)
+{
+	if(isEmpty(root))
+		return INT_MIN;
+	NodeStack *node = createNode(data);
+	(*root)->next = node;
+	*root = node;
+}
+
+void sortLinkedListStack(NodeStack* root)
+{
+	if(!root)
+		return INT_MIN;
+	
+}
